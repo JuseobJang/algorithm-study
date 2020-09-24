1bc2aa687c5ef60be781b575d116bae7a2e40a07
JuseobJang <wntjq68@gmail.com>
2020-09-24 23:27:17 +0900

    feat: Upload 2309

---
+++ CodePlus/SW 역량테스트 준비 -기초 : 브루트포스/2309/Main.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    static int[] nan = new int[9];
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+        for (int i = 0; i < 9; i++) {
+            nan[i] = Integer.parseInt(br.readLine());
+            sum += nan[i];
+        }
+        Arrays.sort(nan);
+
+        for (int i = 0; i < 9; i++) {
+            for (int j = i + 1; j < 9; j++) {
+                if (sum - nan[i] - nan[j] == 100) {
+                    for (int k = 0; k < 9; k++) {
+                        if (k == i || k == j) {
+                            continue;
+                        } else {
+                            System.out.println(nan[k]);
+                        }
+                    }
+                    return;
+                }
+            }
+        }
+    }
+}
