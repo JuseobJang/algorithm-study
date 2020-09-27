25536acc1d22821701ef37d976d57a01471a029a
JuseobJang <wntjq68@gmail.com>
2020-09-27 14:41:23 +0900

    feat: Upload 14501

---
+++ CodePlus/SW 역량테스트 준비 -기초 : 브루트포스/14501/Main.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    static int[] T;
+    static int[] P;
+    static int n;
+    static int max = 0;
+
+    static void bfs(int day, int pay) {
+        if (day == n) {
+            max = Math.max(max, pay);
+            return;
+        }
+        if (day + T[day] - 1 < n) {
+            bfs(day + T[day], pay + P[day]);
+        }
+        bfs(day + 1, pay);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        T = new int[n];
+        P = new int[n];
+        for (int i = 0; i < n; i++) {
+            String[] input = br.readLine().split(" ");
+            T[i] = Integer.parseInt(input[0]);
+            P[i] = Integer.parseInt(input[1]);
+        }
+        bfs(0, 0);
+        System.out.println(max);
+    }
+}
