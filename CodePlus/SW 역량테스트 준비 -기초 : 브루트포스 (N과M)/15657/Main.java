9cfea8d24467a2b38847613f0fb3f1dbab741468
JuseobJang <wntjq68@gmail.com>
2020-10-09 17:05:51 +0900

    feat: Upload 15657

---
+++ CodePlus/SW 역량테스트 준비 -기초 : 브루트포스 (N과M)/15657/Main.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+
+public class Main {
+    static int n;
+    static int m;
+    static int[] arr;
+    static int[] sol;
+    static BufferedReader br;
+    static BufferedWriter bw;
+
+    static void dfs(int depth, int nextNum) throws Exception {
+        sol[depth] = nextNum;
+
+        if (depth == m - 1) {
+            for (int i = 0; i < m; i++) {
+                bw.write(Integer.toString(sol[i]) + " ");
+            }
+            bw.newLine();
+            return;
+        }
+        for (int i = 0; i < n; i++) {
+            if (arr[i] >= sol[depth])
+                dfs(depth + 1, arr[i]);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+
+        String[] input2 = br.readLine().split(" ");
+
+        arr = new int[n];
+        sol = new int[m];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(input2[i]);
+        }
+        Arrays.sort(arr);
+
+        for (int i = 0; i < n; i++) {
+            dfs(0, arr[i]);
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
