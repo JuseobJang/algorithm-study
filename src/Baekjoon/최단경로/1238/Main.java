2febcef28a21cd188e84ae3c9195b7d12d7ff034
JuseobJang <wntjq68@gmail.com>
2021-04-13 18:01:49 +0900

    feat: Upload 1238

---
+++ src/Baekjoon/최단경로/1238/Main.java
@@ -0,0 +1,65 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+    static BufferedReader br;
+    static BufferedWriter bw;
+    static int n;
+    static int m;
+    static int x;
+    static int[][] dist;
+    static int INF = 10_000_000;
+
+    public static void main(String[] args) throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        String[] input = br.readLine().split(" ");
+        n = Integer.parseInt(input[0]);
+        m = Integer.parseInt(input[1]);
+        x = Integer.parseInt(input[2]) - 1;
+        dist = new int[n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == j)
+                    dist[i][j] = 0;
+                else
+                    dist[i][j] = INF;
+            }
+        }
+
+        for (int i = 0; i < m; i++) {
+            input = br.readLine().split(" ");
+            int start = Integer.parseInt(input[0]) - 1;
+            int end = Integer.parseInt(input[1]) - 1;
+            int cost = Integer.parseInt(input[2]);
+            dist[start][end] = cost;
+        }
+
+        floydWarshall();
+
+        int ans = -1;
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dist[i][x] + dist[x][i]);
+        }
+
+        bw.write(Integer.toString(ans));
+        bw.flush();
+
+        br.close();
+        bw.close();
+    }
+
+    static void floydWarshall() {
+        for (int k = 0; k < n; k++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    dist[i][j] = Math.min(dist[i][j], dist[i][k] + dist[k][j]);
+                }
+            }
+        }
+    }
+}
